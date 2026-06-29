181bed8b17e52af8c101a5bbc6ee17fb066cb268
Ameer <your_email@example.com>
2026-06-29 10:36:30 +0530

    restructured all and commiting at once

--- Cloud/Terraform/AWS/vpc-test/main.tf
+++
@@ -1,7 +0,0 @@
-module "vpc" {
-    source = "../modules/vpc"
-    #source = "git::https://github.com/daws-90s/terraform-aws-vpc.git?ref=main"
-    project = var.project
-    environment = var.environment
-    
-}
